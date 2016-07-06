1ee63608ed9cba62cbfd9e66bb63fe119988c489
Vladimir Astachkine <vladimir.astachkine@gmail.com>
2016-07-06 14:08:32 +0300

    doctors005

--- tmdx/database/methods/Person/Person-events.js
+++ tmdx/database/methods/Person/Person-events.js
@@ -1,12 +1,20 @@
 
 
-model.Person.events.init = function(event) {
-	
-	this.patient = true;
-	this.doctor = false;
+model.Person.events.init = function(event) {	
 	this.male = true;
 	this.female = false;
-	this.clinicboss = false;
-	this.clinicadmin = false;
-	
+	this.ispatient = true;
+	this.isdoctor = false;
+	this.isclinicboss = false;
+	this.isclinicadmin = false;
+};
+
+
+model.Person.events.load = function(event) {
+	// Add your code here;
+};
+
+
+model.Person.birth.events.init = function(event) {
+	// Add your code here;
 };
--- tmdx/web/prototype/adminpanel.waPage/scripts/index.js
+++ tmdx/web/prototype/adminpanel.waPage/scripts/index.js
@@ -2,11 +2,17 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
 	var cboxFemale = {};	// @Checkbox
 	var cboxMale = {};	// @Checkbox
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		waf.sources.person.query("ispatient = :1", true);
+	};// @lock
 
 	cboxFemale.click = function cboxFemale_click (event)// @startlock
 	{// @endlock
@@ -39,6 +45,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("cboxFemale", "click", cboxFemale.click, "WAF");
 	WAF.addListener("cboxMale", "click", cboxMale.click, "WAF");
 	WAF.addListener("cboxFemale", "change", cboxFemale.change, "WAF");
